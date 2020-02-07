99888affeaa53d52f8d129b82a856090a644aa18
travisalongi <50125469+travisalongi@users.noreply.github.com>
2020-02-07 13:34:43 -0800

    initial
    a lot of work put into formatting times correctly for the plot.

---
+++ anss_pi_event_timing.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 10:01:21 2020
+
+@author: Travis Alongi
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+from obspy.core import UTCDateTime
+plt.close()
+
+with open('ANSS_1980-2019.csv') as f:
+    header_names = f.readline().split(',')
+        
+catalog = pd.read_csv('anss_pi_events.txt', sep = '\s+', names = header_names )
+
+utc_time = [UTCDateTime(time) for time in catalog.time]
+time_mpl = np.array([UTCDateTime(time).matplotlib_date for time in catalog.time])
+
+limits = np.linspace(time_mpl.min(), time_mpl.max(), 5)
+limits = limits.round() #round to integer
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+years_fmt = mdates.DateFormatter('%Y')
+
+fig, ax = plt.subplots(1,1)
+mask_0 = np.logical_and(time_mpl > limits[0], time_mpl < limits[1])
+mask_1 = np.logical_and(time_mpl > limits[1], time_mpl < limits[2])
+mask_2 = np.logical_and(time_mpl > limits[2], time_mpl < limits[3])
+mask_3 = np.logical_and(time_mpl > limits[3], time_mpl < limits[4])
+
+ax.scatter(time_mpl, catalog.depth/1e3, catalog.mag**3, 'k', alpha = 0.5)
+
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.set_xlim([limits.min(), limits.max()])
+ax.tick_params(axis = 'x', labelsize = 6)
+fig.autofmt_xdate()
+fig.tight_layout()
+ax.set_ylabel('Depth [km]')
