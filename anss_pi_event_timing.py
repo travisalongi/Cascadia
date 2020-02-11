1695756b7a11a10f84dc0d86daf4aa45838180c8
travisalongi <50125469+travisalongi@users.noreply.github.com>
2020-02-10 17:43:07 -0800

    added some machine learning clustering and plot
    plot includes depth vs latitude

--- anss_pi_event_timing.py
+++ anss_pi_event_timing.py
@@ -25,7 +25,7 @@ limits = limits.round() #round to integer
 
 years = mdates.YearLocator()   # every year
 months = mdates.MonthLocator()  # every month
-years_fmt = mdates.DateFormatter('%Y')
+years_fmt = mdates.DateFormatter('%Y-%m')
 
 fig, ax = plt.subplots(1,1)
 mask_0 = np.logical_and(time_mpl > limits[0], time_mpl < limits[1])
@@ -34,6 +34,37 @@ mask_2 = np.logical_and(time_mpl > limits[2], time_mpl < limits[3])
 mask_3 = np.logical_and(time_mpl > limits[3], time_mpl < limits[4])
 
 ax.scatter(time_mpl, catalog.depth/1e3, catalog.mag**3, 'k', alpha = 0.5)
+
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.set_xlim([limits.min(), limits.max()])
+ax.tick_params(axis = 'x', labelsize = 6)
+fig.autofmt_xdate()
+fig.tight_layout()
+ax.set_ylabel('Depth [km]')
+
+
+fig, ax = plt.subplots(1,1)
+mask_0 = np.logical_and(time_mpl > limits[0], time_mpl < limits[1])
+mask_1 = np.logical_and(time_mpl > limits[1], time_mpl < limits[2])
+mask_2 = np.logical_and(time_mpl > limits[2], time_mpl < limits[3])
+mask_3 = np.logical_and(time_mpl > limits[3], time_mpl < limits[4])
+
+ax.scatter(time_mpl, catalog.latitude, catalog.mag**3, 'k', alpha = 0.5)
+winsize = 200
+plt.plot(time_mpl, catalog.latitude.rolling(winsize).median(), 'r-')
+plt.plot(time_mpl, catalog.latitude.rolling(winsize).median() + catalog.latitude.rolling(winsize).std(), 'r:')
+plt.plot(time_mpl, catalog.latitude.rolling(winsize).median() - catalog.latitude.rolling(winsize).std(), 'r:')
+
+
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+kfit = KMeans(n_clusters = 4).fit(np.column_stack((time_mpl, catalog.latitude)))
+kfit_centroids = kfit.cluster_centers_
+t, la = kfit_centroids.T
+plt.plot(t,la,'g*')
+
+gfit = GaussianMixture().fit_predict(np.column_stack((time_mpl, catalog.latitude)))
 
 ax.xaxis.set_major_locator(years)
 ax.xaxis.set_major_formatter(years_fmt)
