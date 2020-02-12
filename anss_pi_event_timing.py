c5cc3d9601ccc97c1b7cd4107f358fa39d78612f
travisalongi <50125469+travisalongi@users.noreply.github.com>
2020-02-12 13:08:04 -0800

    Add files via upload
    added comments to script and added figure latitude vs time

--- anss_pi_event_timing.py
+++ anss_pi_event_timing.py
@@ -27,12 +27,14 @@ years = mdates.YearLocator()   # every year
 months = mdates.MonthLocator()  # every month
 years_fmt = mdates.DateFormatter('%Y-%m')
 
-fig, ax = plt.subplots(1,1)
-mask_0 = np.logical_and(time_mpl > limits[0], time_mpl < limits[1])
-mask_1 = np.logical_and(time_mpl > limits[1], time_mpl < limits[2])
-mask_2 = np.logical_and(time_mpl > limits[2], time_mpl < limits[3])
-mask_3 = np.logical_and(time_mpl > limits[3], time_mpl < limits[4])
+# ** these mask are not used in this code, left over from debugging, maybe useful in future
+#mask_0 = np.logical_and(time_mpl > limits[0], time_mpl < limits[1])
+#mask_1 = np.logical_and(time_mpl > limits[1], time_mpl < limits[2])
+#mask_2 = np.logical_and(time_mpl > limits[2], time_mpl < limits[3])
+#mask_3 = np.logical_and(time_mpl > limits[3], time_mpl < limits[4])
 
+# fig1 time vs. depth.
+fig, ax = plt.subplots(1,1)
 ax.scatter(time_mpl, catalog.depth/1e3, catalog.mag**3, 'k', alpha = 0.5)
 
 ax.xaxis.set_major_locator(years)
@@ -44,27 +46,11 @@ fig.tight_layout()
 ax.set_ylabel('Depth [km]')
 
 
+#fig 2 time vs latitude
 fig, ax = plt.subplots(1,1)
-mask_0 = np.logical_and(time_mpl > limits[0], time_mpl < limits[1])
-mask_1 = np.logical_and(time_mpl > limits[1], time_mpl < limits[2])
-mask_2 = np.logical_and(time_mpl > limits[2], time_mpl < limits[3])
-mask_3 = np.logical_and(time_mpl > limits[3], time_mpl < limits[4])
-
 ax.scatter(time_mpl, catalog.latitude, catalog.mag**3, 'k', alpha = 0.5)
 winsize = 200
-plt.plot(time_mpl, catalog.latitude.rolling(winsize).median(), 'r-')
-plt.plot(time_mpl, catalog.latitude.rolling(winsize).median() + catalog.latitude.rolling(winsize).std(), 'r:')
-plt.plot(time_mpl, catalog.latitude.rolling(winsize).median() - catalog.latitude.rolling(winsize).std(), 'r:')
-
-
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
-kfit = KMeans(n_clusters = 4).fit(np.column_stack((time_mpl, catalog.latitude)))
-kfit_centroids = kfit.cluster_centers_
-t, la = kfit_centroids.T
-plt.plot(t,la,'g*')
-
-gfit = GaussianMixture().fit_predict(np.column_stack((time_mpl, catalog.latitude)))
+#plt.plot(time_mpl, catalog.latitude.rolling(winsize).median(), 'r-')
 
 ax.xaxis.set_major_locator(years)
 ax.xaxis.set_major_formatter(years_fmt)
@@ -72,4 +58,4 @@ ax.set_xlim([limits.min(), limits.max()])
 ax.tick_params(axis = 'x', labelsize = 6)
 fig.autofmt_xdate()
 fig.tight_layout()
-ax.set_ylabel('Depth [km]')
+ax.set_ylabel('Latitude')
